568ad252db8333671200184a1c9bca46aa1432f0
Tzu-Kuan Hung <token@teddy.cs.nthu.edu.tw>
2021-11-07 14:12:00 +0800

    remove test.patch

--- setup_env_ds.sh
+++ setup_env_ds.sh
@@ -77,6 +77,5 @@ patch -sp0 < libgdiam-1.0.3.patch
 cd libgdiam-1.0.3
 mkdir build && cd build && cmake .. && make
 cd ../../../..
-patch -u algorithms/PCGCv2/test.py test.patch
 mv checkpoints/ algorithms/PCGCv1/
 mv ckpts/ algorithms/PCGCv2/
